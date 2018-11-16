1b178f05f1e9c25f00f92b06d43ff271022916c8
JewsOfHazard <logan.saso2016@gmail.com>
2018-11-15 17:15:49 -0800

    Initial commit

---
+++ otp_enc_d/src/main.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+
+use rand::prelude::*;
+use std::env::args;
+use std::net::Ipv4Addr;
+
+fn main() {
+    let port = args()
+        .skip(1)
+        .next()
+        .expect("Usage: otp_enc_d <port>")
+        .parse::<u32>()
+        .expect("Couldn't parse port into valid u32.");
+
+    if port > 65536 {
+        eprintln!("The port must be between 1 and 65536 inclusive.");
+        return;
+    }
+
+    let url = String::from("127.0.0.1");
+
+}
